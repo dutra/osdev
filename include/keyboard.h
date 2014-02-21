4354083c9e808a83792eb5937b0c0c211a0e6ed7
Isaque Dutra <dutra@mit.edu>
2014-02-21 06:55:41 -0500

    Updated keyboard driver. Defined Scan Code macros and a simple mapping for ASCII

--- include/keyboard.h
+++ include/keyboard.h
@@ -3,4 +3,75 @@
 
 void init_keyboard();
 
+// Keyboard Scan Codes
+#define KEY_ESC 1
+#define KEY_1 2
+#define KEY_2 3
+#define KEY_3 4
+#define KEY_4 5
+#define KEY_5 6
+#define KEY_6 7
+#define KEY_7 8
+#define KEY_8 9
+#define KEY_9 10
+#define KEY_0 11
+#define KEY_HYPHEN 12
+#define KEY_EQUALS 13
+#define KEY_BACKSPACE 14
+#define KEY_TAB 15
+#define KEY_Q 16
+#define KEY_W 17
+#define KEY_E 18
+#define KEY_R 19
+#define KEY_T 20
+#define KEY_Y 21
+#define KEY_U 22
+#define KEY_I 23
+#define KEY_O 24
+#define KEY_P 25
+#define KEY_LEFT_BRACKET 26
+#define KEY_RIGHT_BRACKET 27
+#define KEY_ENTER 28
+#define KEY_LEFT_CTRL 29
+#define KEY_A 30
+#define KEY_S 31
+#define KEY_D 32
+#define KEY_F 33
+#define KEY_G 34
+#define KEY_H 35
+#define KEY_J 36
+#define KEY_K 37
+#define KEY_L 38
+#define KEY_SEMICOLON 39
+#define KEY_ASP 40
+#define KEY_TILT 41
+#define KEY_LEFT_SHIFT 42
+#define KEY_Z 44
+#define KEY_X 45
+#define KEY_C 46
+#define KEY_V 47
+#define KEY_B 48
+#define KEY_N 49
+#define KEY_M 50
+#define KEY_COMMA 51
+#define KEY_PERIOD 52
+#define KEY_BACK_SLASH 53
+#define KEY_RIGHT_SHIFT 54
+#define KEY_STAR 55
+#define KEY_LEFT_ALT 56
+#define KEY_SPACEBAR 57
+#define KEY_CAPSLOCK 58
+#define KEY_F1 59
+#define KEY_F2 60
+#define KEY_F3 61
+#define KEY_F4 62
+#define KEY_F5 63
+#define KEY_F6 64
+#define KEY_F7 65
+#define KEY_F8 66
+#define KEY_F9 67
+#define KEY_F10 68
+#define KEY_F11 87
+#define KEY_F12 88
+
 #endif /* KEYBOARD_H */
--- keyboard.c
+++ keyboard.c
@@ -2,21 +2,70 @@
 #include <screen.h>
 #include <asm.h>
 #include <lib.h>
+#include <keyboard.h>
 
+// contains mapping from scancodes to ascii
+uint8_t scancode[88];
 
 static void keyboard_callback(registers_t regs) {
+
   uint8_t keycode;
   char keycode_string[10];
+  char key[4];
   
   keycode = (uint8_t) inb(0x60);
+
+  if(keycode & 0x80) {
+    print_string("Key lifted: ");
+    keycode -= 0x80;
+  } else
+    print_string("Key pressed: ");
+
+  key[0] = scancode[keycode];
+  key[1] = '\0';
+
   
-  print_string("Key pressed: ");
+  print_string(itoa(scancode[keycode],keycode_string, 10));
+  print_string("  ");
+  print_string(key);
+  print_string("  ");
   print_string(itoa(keycode, keycode_string, 10));
   print_string("\n");
+
 }
 
+// initializes the scancodes mapping and registers our keyboard handler
 void init_keyboard() {
-  
+
+    /* less efficient than scancode[] = {}, but easier to catch a bug. Will change later */
+  scancode[KEY_A] = 'a';
+  scancode[KEY_B] = 'b';
+  scancode[KEY_C] = 'c';
+  scancode[KEY_D] = 'd';
+  scancode[KEY_E] = 'e';
+  scancode[KEY_F] = 'f';
+  scancode[KEY_G] = 'g';
+  scancode[KEY_H] = 'h';
+  scancode[KEY_I] = 'i';
+  scancode[KEY_J] = 'j';
+  scancode[KEY_K] = 'k';
+  scancode[KEY_L] = 'l';
+  scancode[KEY_M] = 'm';
+  scancode[KEY_N] = 'n';
+  scancode[KEY_O] = 'o';
+  scancode[KEY_P] = 'p';
+  scancode[KEY_Q] = 'q';
+  scancode[KEY_R] = 'r';
+  scancode[KEY_S] = 's';
+  scancode[KEY_T] = 't';
+  scancode[KEY_U] = 'u';
+  scancode[KEY_V] = 'v';
+  scancode[KEY_X] = 'x';
+  scancode[KEY_W] = 'w';
+  scancode[KEY_Y] = 'y';
+  scancode[KEY_Z] = 'z';
+
   register_interrupt_handler(IRQ1, &keyboard_callback);
+
 
 }
